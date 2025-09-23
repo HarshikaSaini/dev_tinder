e4a034e2d3fbf91b6e8d972864dd80c6b3830591
HarshikaSaini <harshikasaini1415@gmail.com>
2025-09-23 23:55:01 +0530

    Added forgot password route

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -1,7 +1,7 @@
-const express = require("express")
+const express = require("express");
 const profileRouter = express.Router();
-const {userAuth,userEditData} = require("../utils/user")
-
+const { userAuth, userEditData } = require("../utils/user");
+const bcrypt = require("bcrypt");
 
 profileRouter.get("/profile/view", userAuth, (req, res) => {
   try {
@@ -12,30 +12,52 @@ profileRouter.get("/profile/view", userAuth, (req, res) => {
   }
 });
 
-profileRouter.patch("/profile/edit", userAuth , async (req, res) => {
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
   try {
-    if(!userEditData(req)){
-        throw new Error("Invalid edit request");
+    if (!userEditData(req)) {
+      throw new Error("Invalid edit request");
     }
     const loggedInUser = req.user;
-    Object.keys(req.body).forEach(key => loggedInUser[key] = req.body[key])
-    await loggedInUser.save()
-    res.status(200).json({mess:"User updated sucessfull", data:loggedInUser})
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+    await loggedInUser.save();
+    res
+      .status(200)
+      .json({ mess: "User updated sucessfull", data: loggedInUser });
   } catch (error) {
     console.log(error);
     res.status(500).send("internal server error");
   }
 });
 
-profileRouter.delete("/profile/remove", userAuth , async (req, res) => {
+profileRouter.delete("/profile/remove", userAuth, async (req, res) => {
   try {
     const user = req.user;
     await user.deleteOne();
-    res.status(200).json({mess:"User deleted sucessfully", data:user})
+    res.status(200).json({ mess: "User deleted sucessfully", data: user });
   } catch (error) {
     console.log(error);
     res.status(500).send("internal server error");
   }
 });
 
-module.exports = profileRouter
+profileRouter.patch("/profile/forgot-password", userAuth, async (req, res) => {
+  try {
+    const { password, newPassword } = req.body;
+    const user = req.user;
+    const isValidPass = await user.validatePassword(password);
+    
+    if (!isValidPass) {
+      throw new Error("Enter correct password");
+    }
+    else{
+      const hashPassword = await bcrypt.hash(newPassword,10)
+      await user.updateOne({password:hashPassword})
+      res.status(200).send("Password changed successfully")
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).send("Internal server error",error)
+  }
+});
+
+module.exports = profileRouter;
